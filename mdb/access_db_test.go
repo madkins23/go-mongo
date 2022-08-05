e2204b0e020738003471a424cbe7b96bf6cd647c
mAdkins <software@doorways.org>
2022-08-04 18:50:14 -0700

    Move access_db_panic_test into access_db_test

--- mdb/access_db_test.go
+++ mdb/access_db_test.go
@@ -6,10 +6,20 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type accessTestSuite struct {
 	AccessTestSuite
+}
+
+func (suite *accessTestSuite) TestConnectOrPanic() {
+	// Cause a failure by using a bad URI.
+	opts := options.Client()
+	opts.ApplyURI("bad URI")
+	suite.Panics(func() {
+		ConnectOrPanic("noSuchDB", &Config{Options: opts})
+	}, "TestConnectOrPanic did not panic")
 }
 
 func TestAccessSuite(t *testing.T) {
@@ -22,4 +32,18 @@ func (suite *accessTestSuite) TestPing() {
 
 func (suite *accessTestSuite) TestContext() {
 	suite.Require().NotNil(suite.access.Context())
+}
+
+func (suite *accessTestSuite) DontTestDisconnectOrPanic() {
+	// TODO(mAdkins): is there a way to force a disconnect failure?
+	//  Forcing the disconnect timeout to zero doesn't work.
+	oldTimeout := DefaultDisconnectTimeout
+	DefaultDisconnectTimeout = 0
+	defer func() { DefaultDisconnectTimeout = oldTimeout }()
+	access, err := Connect(AccessTestDBname, nil)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(access)
+	suite.Panics(func() {
+		access.DisconnectOrPanic()
+	}, "TestConnectOrPanic did not panic")
 }
