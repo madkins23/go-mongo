7b5f1913d7dcbb5c14360d38f8c3b571b2a2ca53
mAdkins <software@doorways.org>
2022-08-17 10:56:54 -0700

    Add sync.Mutex around CachedCollection cache references without any testing

--- mdb/cached_collection.go
+++ mdb/cached_collection.go
@@ -3,6 +3,7 @@ package mdb
 import (
 	"fmt"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/madkins23/go-utils/check"
@@ -13,12 +14,12 @@ import (
 // CachedCollection caches Mongo-stored objects so that the same object is always returned.
 // This is most useful for objects that change rarely.
 //
-// TODO(mAdkins): Should this be made thread-safe?
 // TODO(mAdkins): Should the change stream be used to clear cache items?
 type CachedCollection[C Cacheable] struct {
 	TypedCollection[C]
 	cache       map[string]C
 	expireAfter time.Duration
+	lock        sync.Mutex
 }
 
 // ConnectCachedCollection creates a new cached collection object with the specified collection definition.
@@ -56,14 +57,17 @@ type Searchable interface {
 // Delete object in cache and DB.
 // Because items are Cacheable (and therefore Searchable) the item itself is passed instead of a filter.
 func (c *CachedCollection[Cacheable]) Delete(item Searchable, idempotent bool) error {
+	c.lock.Lock()
 	delete(c.cache, item.CacheKey())
+	c.lock.Unlock()
 	return c.Collection.Delete(item.Filter(), idempotent)
 }
 
 // DeleteAll all objects in cache and DB.
 func (c *CachedCollection[Cacheable]) DeleteAll() error {
-	// TODO(mAdkins): Why does this compile when c.cache is defined as map[string]C in the struct?
+	c.lock.Lock()
 	c.cache = make(map[string]Cacheable)
+	c.lock.Unlock()
 	return c.Collection.DeleteAll()
 }
 
@@ -71,6 +75,8 @@ func (c *CachedCollection[Cacheable]) DeleteAll() error {
 func (c *CachedCollection[C]) Find(searchFor Searchable) (C, error) {
 	var item C
 	var found bool
+	c.lock.Lock()
+	defer c.lock.Unlock()
 	cacheKey := searchFor.CacheKey()
 	if item, found = c.cache[cacheKey]; found {
 		remove := false
@@ -132,11 +138,13 @@ func (c *CachedCollection[Cacheable]) FindOrCreate(cacheItem Cacheable) (Cacheab
 
 // InvalidateByPrefix removes items from the cache if the item key has the specified prefix.
 func (c *CachedCollection[T]) InvalidateByPrefix(prefix string) {
+	c.lock.Lock()
 	for k := range c.cache {
 		if strings.HasPrefix(k, prefix) {
 			delete(c.cache, k)
 		}
 	}
+	c.lock.Unlock()
 }
 
 // Iterate over a set of items, applying the specified function to each one.
@@ -164,18 +172,6 @@ func (c *CachedCollection[T]) Iterate(filter bson.D, fn func(item T) error) erro
 // the filter here must be a typed item in order to properly clear the cache entry.
 func (c *CachedCollection[T]) Replace(filter, item T, opts ...*options.UpdateOptions) error {
 	return c.Update(filter, bson.M{"$set": item}, opts...)
-
-	//err := c.Collection.Replace(filter.Filter(), item, opts...)
-	//if err != nil {
-	//	return fmt.Errorf("basic replace: %w", err)
-	//}
-	//
-	//itemKey := item.CacheKey()
-	//delete(c.cache, itemKey)
-	//if filterKey := filter.CacheKey(); filterKey != itemKey {
-	//	delete(c.cache, filterKey)
-	//}
-	//return nil
 }
 
 // Update item referenced by filter by applying update operator expressions.
@@ -187,7 +183,9 @@ func (c *CachedCollection[T]) Update(filter T, changes interface{}, opts ...*opt
 	}
 
 	filterKey := filter.CacheKey()
+	c.lock.Lock()
 	delete(c.cache, filterKey)
+	c.lock.Unlock()
 
 	// If the changes affect the cache key there is no convenient way to figure
 	// out what the new cache key would be in order to delete it from the cache.
